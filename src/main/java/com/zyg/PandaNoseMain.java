fcb150718e3a97c9973a3d4c4a204e663d7a0dd9
michael <michael_zhangyg@icloud.com>
2021-09-05 11:59:22 +0800

    add first class

---
+++ src/main/java/com/zyg/PandaNoseMain.java
@@ -0,0 +1,14 @@
+package com.zyg;
+
+/**
+ * @author ：michael
+ * @date ：2021/9/5 11:56 AM
+ * @description：分布式链路属性追踪器
+ * @modified By：michael
+ * @version: V 0.0.1
+ */
+public class PandaNoseMain {
+    public static void main(String[] args) {
+        System.out.println("hello panda nose");
+    }
+}
